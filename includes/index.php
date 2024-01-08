22e8e4b4a2a9643fae1e70d571cbb673fd7539d9
rania.fathy <rania.fathy@objects.ws>
2024-01-08 19:03:19 +0200

    enhance code

--- includes/index.php
+++ includes/index.php
@@ -2,7 +2,3 @@
 include_once 'settings/index.php';
 include_once 'general/index.php';
 include_once 'next-payment/index.php';
-include_once 'skip-shipment/index.php';
-include_once 'wcs-all-frequency/index.php';
-//include_once 'wcs-frequency/index.php';
-include_once 'wc-sub-box/index.php';
